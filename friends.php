e092338e40ca12823b709ce789da115cf4b550e8
Nihar Sanda <nihar.sanda@gmail.com>
2021-06-12 17:43:10 +0530

    formatted the code

--- friends.php
+++ friends.php
@@ -1,10 +1,9 @@
 <?php
 include("includes/header.php");
 
-if(isset($_GET['username'])) {
+if (isset($_GET['username'])) {
 	$username = $_GET['username'];
-}
-else {
+} else {
 	$username = $userLoggedIn;
 }
 ?>
@@ -12,15 +11,15 @@ else {
 <div class="main_column column" id="main_column">
 
 	<?php
-		$user_obj = new User($con, $username);
-		foreach($user_obj->getFriendsList() as $friend) {
-			$friend_obj = new User($con, $friend);
-			echo "<a href='$friend'>
-					<img class='profilePicSmall' src='" . $friend_obj->getProfilePic() ."'>"
-					 . $friend_obj->getFirstAndLastName() . 
-				"</a>
+	$user_obj = new User($con, $username);
+	foreach ($user_obj->getFriendsList() as $friend) {
+		$friend_obj = new User($con, $friend);
+		echo "<a href='$friend'>
+					<img class='profilePicSmall' src='" . $friend_obj->getProfilePic() . "'>"
+			. $friend_obj->getFirstAndLastName() .
+			"</a>
 				<br>";
-		}
+	}
 	?>
 
 </div>
--- includes/header.php
+++ includes/header.php
@@ -1,4 +1,4 @@
-<?php  
+<?php
 require 'config/config.php';
 include("includes/classes/User.php");
 include("includes/classes/Post.php");
@@ -10,13 +10,13 @@ if (isset($_SESSION['username'])) {
 	$userLoggedIn = $_SESSION['username'];
 	$user_details_query = mysqli_query($con, "SELECT * FROM users WHERE username='$userLoggedIn'");
 	$user = mysqli_fetch_array($user_details_query);
-}
-else {
+} else {
 	header("Location: register.php");
 }
 
 ?>
 <html>
+
 <head>
 	<title>Welcome to HindBook</title>
 
@@ -29,16 +29,18 @@ else {
 	<script src="assets/js/jquery.Jcrop.js"></script>
 	<script src="assets/js/jcrop_bits.js"></script>
 
-
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	
 	<!-- CSS -->
 	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
 	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
 	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
 	<link rel="stylesheet" href="assets/css/jquery.Jcrop.css" type="text/css" />
 </head>
+
 <body>
 
-	<div class="top_bar"> 
+	<div class="top_bar">
 
 		<div class="logo">
 			<a href="index.php">HindBook!</a>
@@ -68,17 +70,17 @@ else {
 
 		<nav>
 			<?php
-				//Unread messages 
-				$messages = new Message($con, $userLoggedIn);
-				$num_messages = $messages->getUnreadNumber();
+			//Unread messages 
+			$messages = new Message($con, $userLoggedIn);
+			$num_messages = $messages->getUnreadNumber();
 
-				//Unread notifications 
-				$notifications = new Notification($con, $userLoggedIn);
-				$num_notifications = $notifications->getUnreadNumber();
+			//Unread notifications 
+			$notifications = new Notification($con, $userLoggedIn);
+			$num_notifications = $notifications->getUnreadNumber();
 
-				//Unread notifications 
-				$user_obj = new User($con, $userLoggedIn);
-				$num_requests = $user_obj->getNumberOfFriendRequests();
+			//Unread notifications 
+			$user_obj = new User($con, $userLoggedIn);
+			$num_requests = $user_obj->getNumberOfFriendRequests();
 			?>
 
 
@@ -91,22 +93,22 @@ else {
 			<a href="javascript:void(0);" onclick="getDropdownData('<?php echo $userLoggedIn; ?>', 'message')">
 				<i class="fa fa-envelope fa-lg"></i>
 				<?php
-				if($num_messages > 0)
-				 echo '<span class="notification_badge" id="unread_message">' . $num_messages . '</span>';
+				if ($num_messages > 0)
+					echo '<span class="notification_badge" id="unread_message">' . $num_messages . '</span>';
 				?>
 			</a>
 			<a href="javascript:void(0);" onclick="getDropdownData('<?php echo $userLoggedIn; ?>', 'notification')">
 				<i class="fa fa-bell fa-lg"></i>
 				<?php
-				if($num_notifications > 0)
-				 echo '<span class="notification_badge" id="unread_notification">' . $num_notifications . '</span>';
+				if ($num_notifications > 0)
+					echo '<span class="notification_badge" id="unread_notification">' . $num_notifications . '</span>';
 				?>
 			</a>
 			<a href="requests.php">
 				<i class="fa fa-users fa-lg"></i>
 				<?php
-				if($num_requests > 0)
-				 echo '<span class="notification_badge" id="unread_requests">' . $num_requests . '</span>';
+				if ($num_requests > 0)
+					echo '<span class="notification_badge" id="unread_requests">' . $num_requests . '</span>';
 				?>
 			</a>
 			<a href="settings.php">
@@ -126,118 +128,116 @@ else {
 
 
 	<script>
-	// var userLoggedIn = '<?php echo $userLoggedIn; ?>';
+		// var userLoggedIn = '<?php echo $userLoggedIn; ?>';
 
-	// $(document).ready(function() {
+		// $(document).ready(function() {
 
-	// 	$('.dropdown_data_window').scroll(function() {
-	// 		var inner_height = $('.dropdown_data_window').innerHeight(); //Div containing data
-	// 		var scroll_top = $('.dropdown_data_window').scrollTop();
-	// 		var page = $('.dropdown_data_window').find('.nextPageDropdownData').val();
-	// 		var noMoreData = $('.dropdown_data_window').find('.noMoreDropdownData').val();
+		// 	$('.dropdown_data_window').scroll(function() {
+		// 		var inner_height = $('.dropdown_data_window').innerHeight(); //Div containing data
+		// 		var scroll_top = $('.dropdown_data_window').scrollTop();
+		// 		var page = $('.dropdown_data_window').find('.nextPageDropdownData').val();
+		// 		var noMoreData = $('.dropdown_data_window').find('.noMoreDropdownData').val();
 
-	// 		if ((scroll_top + inner_height >= $('.dropdown_data_window')[0].scrollHeight) && noMoreData == 'false') {
+		// 		if ((scroll_top + inner_height >= $('.dropdown_data_window')[0].scrollHeight) && noMoreData == 'false') {
 
-	// 			var pageName; //Holds name of page to send ajax request to
-	// 			var type = $('#dropdown_data_type').val();
-
-
-	// 			if(type == 'notification')
-	// 				pageName = "ajax_load_notifications.php";
-	// 			else if(type == 'message')
-	// 				pageName = "ajax_load_messages.php"
+		// 			var pageName; //Holds name of page to send ajax request to
+		// 			var type = $('#dropdown_data_type').val();
 
 
-	// 			var ajaxReq = $.ajax({
-	// 				url: "includes/handlers/" + pageName,
-	// 				type: "POST",
-	// 				data: "page=" + page + "&userLoggedIn=" + userLoggedIn,
-	// 				cache:false,
-
-	// 				success: function(response) {
-	// 					$('.dropdown_data_window').find('.nextPageDropdownData').remove(); //Removes current .nextpage 
-	// 					$('.dropdown_data_window').find('.noMoreDropdownData').remove(); //Removes current .nextpage 
+		// 			if(type == 'notification')
+		// 				pageName = "ajax_load_notifications.php";
+		// 			else if(type == 'message')
+		// 				pageName = "ajax_load_messages.php"
 
 
-	// 					$('.dropdown_data_window').append(response);
-	// 				}
-	// 			});
+		// 			var ajaxReq = $.ajax({
+		// 				url: "includes/handlers/" + pageName,
+		// 				type: "POST",
+		// 				data: "page=" + page + "&userLoggedIn=" + userLoggedIn,
+		// 				cache:false,
 
-	// 		} //End if 
-
-	// 		return false;
-
-	// 	}); //End (window).scroll(function())
+		// 				success: function(response) {
+		// 					$('.dropdown_data_window').find('.nextPageDropdownData').remove(); //Removes current .nextpage 
+		// 					$('.dropdown_data_window').find('.noMoreDropdownData').remove(); //Removes current .nextpage 
 
 
-	// });
+		// 					$('.dropdown_data_window').append(response);
+		// 				}
+		// 			});
 
+		// 		} //End if 
+
+		// 		return false;
+
+		// 	}); //End (window).scroll(function())
+
+
+		// });
 	</script>
 
 	<script>
-	$(function(){
+		$(function() {
 
-		var userLoggedIn = '<?php echo $userLoggedIn; ?>';
-		var dropdownInProgress = false;
+			var userLoggedIn = '<?php echo $userLoggedIn; ?>';
+			var dropdownInProgress = false;
 
-	    $(".dropdown_data_window").scroll(function() {
-	    	var bottomElement = $(".dropdown_data_window a").last();
-			var noMoreData = $('.dropdown_data_window').find('.noMoreDropdownData').val();
+			$(".dropdown_data_window").scroll(function() {
+				var bottomElement = $(".dropdown_data_window a").last();
+				var noMoreData = $('.dropdown_data_window').find('.noMoreDropdownData').val();
 
-	        // isElementInViewport uses getBoundingClientRect(), which requires the HTML DOM object, not the jQuery object. The jQuery equivalent is using [0] as shown below.
-	        if (isElementInView(bottomElement[0]) && noMoreData == 'false') {
-	            loadPosts();
-	        }
-	    });
-
-	    function loadPosts() {
-	        if(dropdownInProgress) { //If it is already in the process of loading some posts, just return
-				return;
-			}
-			
-			dropdownInProgress = true;
-
-			var page = $('.dropdown_data_window').find('.nextPageDropdownData').val() || 1; //If .nextPage couldn't be found, it must not be on the page yet (it must be the first time loading posts), so use the value '1'
-
-			var pageName; //Holds name of page to send ajax request to
-			var type = $('#dropdown_data_type').val();
-
-			if(type == 'notification')
-				pageName = "ajax_load_notifications.php";
-			else if(type == 'message')
-				pageName = "ajax_load_messages.php";
-
-			$.ajax({
-				url: "includes/handlers/" + pageName,
-				type: "POST",
-				data: "page=" + page + "&userLoggedIn=" + userLoggedIn,
-				cache:false,
-
-				success: function(response) {
-
-					$('.dropdown_data_window').find('.nextPageDropdownData').remove(); //Removes current .nextpage 
-					$('.dropdown_data_window').find('.noMoreDropdownData').remove();
-
-					$('.dropdown_data_window').append(response);
-
-					dropdownInProgress = false;
+				// isElementInViewport uses getBoundingClientRect(), which requires the HTML DOM object, not the jQuery object. The jQuery equivalent is using [0] as shown below.
+				if (isElementInView(bottomElement[0]) && noMoreData == 'false') {
+					loadPosts();
 				}
 			});
-	    }
 
-	    //Check if the element is in view
-	    function isElementInView (el) {
-	        var rect = el.getBoundingClientRect();
+			function loadPosts() {
+				if (dropdownInProgress) { //If it is already in the process of loading some posts, just return
+					return;
+				}
 
-	        return (
-	            rect.top >= 0 &&
-	            rect.left >= 0 &&
-	            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && //* or $(window).height()
-	            rect.right <= (window.innerWidth || document.documentElement.clientWidth) //* or $(window).width()
-	        );
-	    }
-	});
+				dropdownInProgress = true;
 
+				var page = $('.dropdown_data_window').find('.nextPageDropdownData').val() || 1; //If .nextPage couldn't be found, it must not be on the page yet (it must be the first time loading posts), so use the value '1'
+
+				var pageName; //Holds name of page to send ajax request to
+				var type = $('#dropdown_data_type').val();
+
+				if (type == 'notification')
+					pageName = "ajax_load_notifications.php";
+				else if (type == 'message')
+					pageName = "ajax_load_messages.php";
+
+				$.ajax({
+					url: "includes/handlers/" + pageName,
+					type: "POST",
+					data: "page=" + page + "&userLoggedIn=" + userLoggedIn,
+					cache: false,
+
+					success: function(response) {
+
+						$('.dropdown_data_window').find('.nextPageDropdownData').remove(); //Removes current .nextpage 
+						$('.dropdown_data_window').find('.noMoreDropdownData').remove();
+
+						$('.dropdown_data_window').append(response);
+
+						dropdownInProgress = false;
+					}
+				});
+			}
+
+			//Check if the element is in view
+			function isElementInView(el) {
+				var rect = el.getBoundingClientRect();
+
+				return (
+					rect.top >= 0 &&
+					rect.left >= 0 &&
+					rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && //* or $(window).height()
+					rect.right <= (window.innerWidth || document.documentElement.clientWidth) //* or $(window).width()
+				);
+			}
+		});
 	</script>
 
 
