74d5bbb4f807b2dcaec42d3bbe41f8765e8d874c
Nihar Sanda <nihar.sanda@gmail.com>
2021-07-06 18:10:07 +0530

    changed request, settings, upload pag

--- requests.php
+++ requests.php
@@ -2,53 +2,50 @@
 include("includes/header.php"); //Header 
 ?>
 
-<div class="main_column column" id="main_column">
+<main style="margin-top: 40px;">
+	<div class="container">
+		<div class="row">
+			<div class="card shadow p-3 mb-5 bg-white rounded" style="padding: 10px; border-bottom:solid #99DDFF; border-left:solid #99DDFF;">
+				<h4>Friend Requests</h4>
+				<?php
+				$query = mysqli_query($con, "SELECT * FROM friend_requests WHERE user_to='$userLoggedIn'");
+				if (mysqli_num_rows($query) == 0)
+					echo "You have no friend requests at this time!";
+				else {
 
-	<h4>Friend Requests</h4>
+					while ($row = mysqli_fetch_array($query)) {
+						$user_from = $row['user_from'];
+						$user_from_obj = new User($con, $user_from);
 
-	<?php  
+						echo $user_from_obj->getFirstAndLastName() . " sent you a friend request!";
 
-	$query = mysqli_query($con, "SELECT * FROM friend_requests WHERE user_to='$userLoggedIn'");
-	if(mysqli_num_rows($query) == 0)
-		echo "You have no friend requests at this time!";
-	else {
+						$user_from_friend_array = $user_from_obj->getFriendArray();
 
-		while($row = mysqli_fetch_array($query)) {
-			$user_from = $row['user_from'];
-			$user_from_obj = new User($con, $user_from);
+						if (isset($_POST['accept_request' . $user_from])) {
+							$add_friend_query = mysqli_query($con, "UPDATE users SET friend_array=CONCAT(friend_array, '$user_from,') WHERE username='$userLoggedIn'");
+							$add_friend_query = mysqli_query($con, "UPDATE users SET friend_array=CONCAT(friend_array, '$userLoggedIn,') WHERE username='$user_from'");
 
-			echo $user_from_obj->getFirstAndLastName() . " sent you a friend request!";
+							$delete_query = mysqli_query($con, "DELETE FROM friend_requests WHERE user_to='$userLoggedIn' AND user_from='$user_from'");
+							echo "You are now friends!";
+							header("Location: requests.php");
+						}
 
-			$user_from_friend_array = $user_from_obj->getFriendArray();
+						if (isset($_POST['ignore_request' . $user_from])) {
+							$delete_query = mysqli_query($con, "DELETE FROM friend_requests WHERE user_to='$userLoggedIn' AND user_from='$user_from'");
+							echo "Request ignored!";
+							header("Location: requests.php");
+						}
 
-			if(isset($_POST['accept_request' . $user_from ])) {
-				$add_friend_query = mysqli_query($con, "UPDATE users SET friend_array=CONCAT(friend_array, '$user_from,') WHERE username='$userLoggedIn'");
-				$add_friend_query = mysqli_query($con, "UPDATE users SET friend_array=CONCAT(friend_array, '$userLoggedIn,') WHERE username='$user_from'");
-
-				$delete_query = mysqli_query($con, "DELETE FROM friend_requests WHERE user_to='$userLoggedIn' AND user_from='$user_from'");
-				echo "You are now friends!";
-				header("Location: requests.php");
-			}
-
-			if(isset($_POST['ignore_request' . $user_from ])) {
-				$delete_query = mysqli_query($con, "DELETE FROM friend_requests WHERE user_to='$userLoggedIn' AND user_from='$user_from'");
-				echo "Request ignored!";
-				header("Location: requests.php");
-			}
-
-			?>
-			<form action="requests.php" method="POST">
-				<input type="submit" name="accept_request<?php echo $user_from; ?>" id="accept_button" value="Accept">
-				<input type="submit" name="ignore_request<?php echo $user_from; ?>" id="ignore_button" value="Ignore">
-			</form>
-			<?php
-
-
-		}
-
-	}
-
-	?>
-
-
-</div>
+				?>
+						<form action="requests.php" method="POST">
+							<input type="submit" name="accept_request<?php echo $user_from; ?>" id="accept_button" value="Accept">
+							<input type="submit" name="ignore_request<?php echo $user_from; ?>" id="ignore_button" value="Ignore">
+						</form>
+				<?php
+					}
+				}
+				?>
+			</div>
+		</div>
+	</div>
+</main>
--- settings.php
+++ settings.php
@@ -1,53 +1,89 @@
-<?php 
+<?php
 include("includes/header.php");
 include("includes/form_handlers/settings_handler.php");
 ?>
 
-<div class="main_column column">
+<main style="margin-top: 40px;">
+	<div class="container" style="max-width: 600px;">
+		<div class="row">
+			<div class="card shadow p-3 mb-5 bg-white rounded" style="padding: 10px; border-bottom:solid #99DDFF; border-left:solid #99DDFF; align-items:center">
 
-	<h4>Account Settings</h4>
-	<?php
-	echo "<img src='" . $user['profile_pic'] ."' class='small_profile_pic'>";
-	?>
-	<br>
-	<a href="upload.php">Upload new profile picture</a> <br><br><br>
+				<h4>Account Settings</h4>
+				<?php
+				echo "<img class='img-fluid' src='" . $user['profile_pic'] . "'  style='width:100px;height:100px;'>";
+				?>
+				<br>
+				<a href="upload.php">Upload new profile picture</a> <br>
 
-	Modify the values and click 'Update Details'
+				Modify the values and click 'Update Details'
 
-	<?php
-	$user_data_query = mysqli_query($con, "SELECT first_name, last_name, email FROM users WHERE username='$userLoggedIn'");
-	$row = mysqli_fetch_array($user_data_query);
+				<?php
+				$user_data_query = mysqli_query($con, "SELECT first_name, last_name, email FROM users WHERE username='$userLoggedIn'");
+				$row = mysqli_fetch_array($user_data_query);
 
-	$first_name = $row['first_name'];
-	$last_name = $row['last_name'];
-	$email = $row['email'];
-	?>
+				$first_name = $row['first_name'];
+				$last_name = $row['last_name'];
+				$email = $row['email'];
+				?>
+				<div class="col">
+					<form action="settings.php" method="POST">
+						<div class="form-group row">
+							<label for="settings_input" class="col-sm-4 col-form-label">First Name:</label>
+							<div class="col-sm-7">
+								<input type="text" class="form-control" name="first_name" value="<?php echo $first_name; ?>" id="settings_input"><br>
+							</div>
+						</div>
+						<div class="form-group row">
+							<label for="settings_input" class="col-sm-4 col-form-label">Last Name:</label>
+							<div class="col-sm-7">
+								<input type="text" name="last_name" class="form-control" value="<?php echo $last_name; ?>" id="settings_input"><br>
+							</div>
+						</div>
+						<div class="form-group row">
+							<label for="settings_input" class="col-sm-4 col-form-label">Email:</label>
+							<div class="col-sm-7">
+								<input type="text" class="form-control" name="email" value="<?php echo $email; ?>" id="settings_input"><br>
+							</div>
+						</div>
+						<?php echo $message; ?>
 
-	<form action="settings.php" method="POST">
-		First Name: <input type="text" name="first_name" value="<?php echo $first_name; ?>" id="settings_input"><br>
-		Last Name: <input type="text" name="last_name" value="<?php echo $last_name; ?>" id="settings_input"><br>
-		Email: <input type="text" name="email" value="<?php echo $email; ?>" id="settings_input"><br>
-
-		<?php echo $message; ?>
-
-		<input type="submit" name="update_details" id="save_details" value="Update Details" class="info settings_submit"><br>
-	</form>
-
-	<h4>Change Password</h4>
-	<form action="settings.php" method="POST">
-		Old Password: <input type="password" name="old_password" id="settings_input"><br>
-		New Password: <input type="password" name="new_password_1" id="settings_input"><br>
-		New Password Again: <input type="password" name="new_password_2" id="settings_input"><br>
-
-		<?php echo $password_message; ?>
-
-		<input type="submit" name="update_password" id="save_details" value="Update Password" class="info settings_submit"><br>
-	</form>
-
-	<h4>Close Account</h4>
-	<form action="settings.php" method="POST">
-		<input type="submit" name="close_account" id="close_account" value="Close Account" class="danger settings_submit">
-	</form>
-
-
-</div>
+						<button type="submit" class="btn btn-primary" name="update_details" id="save_details" value="Update Details" class="info settings_submit">Update Details</button>
+					</form>
+				</div>
+				<br>
+				<br>
+				<h4>Change Password</h4>
+				<div class="col">
+					<form action="settings.php" method="POST">
+						<div class="form-group row">
+							<label for="settings_input" class="col-sm-4 col-form-label">Old Password:</label>
+							<div class="col-sm-7">
+								<input type="password" class="form-control" name="old_password" id="settings_input"><br>
+							</div>
+						</div>
+						<div class="form-group row">
+							<label for="settings_input" class="col-sm-4 col-form-label">New Password:</label>
+							<div class="col-sm-7">
+								<input type="password" class="form-control" name="new_password_1" id="settings_input"><br>
+							</div>
+						</div>
+						<div class="form-group row">
+							<label for="settings_input" class="col-sm-4 col-form-label">Confirm New Password:</label>
+							<div class="col-sm-7">
+								<input type="password" class="form-control" name="new_password_2" id="settings_input"><br>
+							</div>
+						</div>
+						<?php echo $password_message; ?>
+						<button type="submit" class="btn btn-primary" name="update_password" id="save_details" value="Update Password" class="info settings_submit">Update Password</button><br>
+					</form>
+				</div>
+				<br>
+				<br>
+				<h4>Close Account</h4>
+				<form action="settings.php" method="POST">
+					<button type="submit" class="btn btn-primary" name="close_account" id="close_account" value="Close Account" class="danger settings_submit">Close Account </button>
+				</form>
+			</div>
+		</div>
+	</div>
+</main>
