bed81525e4534db2ebe6a45b8af4cd00b75a34aa
Nihar Sanda <nihar.sanda@gmail.com>
2021-07-07 11:53:12 +0530

    fixed navbar issues

--- includes/header.php
+++ includes/header.php
@@ -56,11 +56,6 @@ if (isset($_SESSION['username'])) {
                 font-size: 17px;
                 position: relative;
             }
-
-            .navbar-items {
-                margin-left: 120px;
-            }
-
         }
 
         @media screen and (min-width: 901px) and (max-width:9000px) {
@@ -81,7 +76,7 @@ if (isset($_SESSION['username'])) {
 
 <body>
     <header>
-        <nav class="navbar navbar-expand-lg navbar-light" style="border-bottom:ridge; border-color:#99DDFF; border-shadow:#99DDFF;">
+        <nav class="navbar navbar-expand-lg navbar-light" style="border-bottom:ridge; border-color:#99DDFF; box-shadow :#99DDFF;">
             <?php
             //Unread messages 
             $messages = new Message($con, $userLoggedIn);
@@ -167,7 +162,10 @@ if (isset($_SESSION['username'])) {
         </nav>
     </header>
 
-
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <!-- Option 2 Separate Popper and Bootstrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
 
     <script>
         $(function() {
